e09573af905f7b9628b0aec216f547084322d60f
CodeOmnister <39503241+CodeOmnister@users.noreply.github.com>
2021-07-20 16:56:44 -0400

    MadLibs Python file added

---
+++ MadLibsScript.py
@@ -0,0 +1,42 @@
+import time
+
+# Story taken from https://www.madtakes.com/libs/184.html because I was too lazy to make my own
+
+print("Welcome to Mad Libs (epic edition)")
+time.sleep(2.5)
+print("Input your responses when prompted.")
+time.sleep(2.5)
+
+verbED = input("VERB ENDING IN -ED: ")
+adjOne = input("ADJECTIVE: ")
+nounPlurOne = input("PLURAL NOUN: ")
+liquid = input("LIQUID: ")
+nounPlurTwo = input("PLURAL NOUN: ")
+famPers = input("FAMOUS PERSON: ")
+place = input("PLACE: ")
+occu = input("OCCUPATION: ")
+nounOne = input("NOUN: ")
+nation = input("NATIONALITY: ")
+femaleCeleb = input("FEMALE CELEBRITY: ")
+nounTwo = input("NOUN: ")
+femFriend = input("FEMALE FRIEND: ")
+nounPlurThree = input("PLURAL NOUN: ")
+num = input("NUMBER: ")
+adjTwo = input("ADJECTIVE: ")
+
+print("\nCalculating Story...\n")
+time.sleep(5)
+print("PERSONAL AD\n")
+print("I enjoy long, " + adjOne + " walks on the beach, getting " + verbED + " in the rain and serendipitous")
+print("encounters with " + nounPlurOne + ". I really like piña coladas mixed with " + liquid + ", and romantic, candle-lit " + nounPlurTwo)
+print("I am well-read from Dr. Seuss to " + famPers + ". I travel frequently, especially to " + place + ", when I am not busy with work.")
+print("(I am a " + occu + " Employee.) I am looking for " + nounOne + " and beauty in the form of a " + nation + " goddess. She should have the physique")
+print("of " + femaleCeleb + " and the " + nounTwo + " of " + femFriend + ". I would prefer if she knew how to cook, clean, and wash my " + nounPlurTwo)
+print("I know I’m not very attractive in my picture, but it was taken " + num +"  days ago, and I have since become more " + adjTwo + ".")
+
+
+
+
+
+
+
